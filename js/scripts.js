0c8cbbeed993eaa58a4e53395f419d4a0879d37f
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-05 15:04:07 -0700

    add function to call other modular function

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,85 @@
 // Business Logic
+
 // Counts up from 0 to the user specified number and stores the count in an array
-function incrementCount(number) {
+function incrementCount(userNumber) {
   var numbersArray = [];
-  for (var i = 0; i <= number; i++) {
+  for (var i = 0; i <= userNumber; i++) {
     numbersArray.push(i);
   }
   return numbersArray;
 }
+
 // Isolates the digits inside of a number and adds them to an array
-function digitSplitter(numbersArray) {
-  for (var i = numbersArray[0]; i < numbersArray.length; i++) {
-    var ones = Math.floor((numbersArray[i] / 1) % 10);
-    var tens = Math.floor((numbersArray[i] / 10) % 10);
-    var hundreds = Math.floor((numbersArray[i] / 100) % 10);
-    var digits = [];
-    digits.push(hundreds);
-    digits.push(tens);
-    digits.push(ones);
-    if ((numbersArray[i] >= 0) && (numbersArray[i] < 10)) {
-      return digits.slice(2);
-    } else if ((numbersArray[i] >= 10) && (numbersArray[i] < 100)) {
-      return digits.slice(1);
-    } else if ((numbersArray[i] >= 100) && (numbersArray[i] < 1000)) {
-      return digits;
-    }
+function digitSplitter(number) {
+  var ones = Math.floor((number / 1) % 10);
+  var tens = Math.floor((number / 10) % 10);
+  var hundreds = Math.floor((number / 100) % 10);
+  var digits = [];
+  digits.push(hundreds);
+  digits.push(tens);
+  digits.push(ones);
+  if ((number >= 0) && (number < 10)) {
+    return digits.slice(2);
+  } else if ((number >= 10) && (number < 100)) {
+    return digits.slice(1);
+  } else if ((number >= 100) && (number < 1000)) {
+    return digits;
   }
 }
 
-// Verifies is an array of sperated digits contains any of the proscribed values to replace
-var specifiedNumber = function isSpecifiedNumber(digits) {
-  return (digits === 3) || (digits === 2) || (digits === 1);
-  };
-console.log(digits.some(specifiedNumber));
+// Return Beeps, Boops, and Sorry's
+function beepBooper(digit) {
+  var digit;
+  if (digit === 3) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if (digit === 2) {
+    return "Boop!";
+  } else if (digit === 1) {
+    return "Beep!";
+  } else {
+    return digit;
+  }
+}
 
-// Return Beeps, Boops, and Sorry's for matching values
-function beepBooper(digits) {
-  for (var i = digits[0]; i < digits.length; i++) {
-    if (digit === 3) {
-      return "I'm sorry, Dave. I'm afraid I can't do that.";
-    } else if (digit === 2) {
-      return "Boop!";
-    } else if (digit === 1) {
-      return "Beep!";
-    } else {
-      return digit;
+function opensPodBayDoors(numbers) {
+  var outputReturn = "";
+  var numbersArray = incrementCount(userNumber);
+  for (var i = 0; i < numbersArray.length; i++) {
+    var number = numbersArray[i];
+    if (number < 10) {
+      return beepBooper(number);
+    }
+    else if ((number >=10) && (number <100)) {
+      for (var d = 0; d < 2; d++) {
+        var digit = digitSplitter[d];
+        if ((digit === 3) || (digit === 2) || (digit === 1)) {
+          return beepBooper(digit);
+        }
+        else {
+          return number;
+        }
+      }
+    }
+    else if ((numbersArray[i] >= 100) && (numbersArray[i] < 1000)) {
+      for (var h = 0; h < 3; h++) {
+        var digit = digitSplitter[h];
+        if ((digit === 3) || (digit === 2) || (digit === 1)) {
+           return beepBooper(digit);
+        } else {
+          return number;
+        }
+      }
     }
   }
 };
 
+console.log(opensPodBayDoors(5))
 
 // User Interface logic
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    var number = $("#input").val();
-
+    var userNumber = $("#input").val();
+    console.log(userNumber)
   });
 });
