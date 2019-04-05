28ac863206af8f070059cbbb31fc291e96e1fc0f
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-05 10:52:19 -0700

    add function to recognize digits 1, 2, 3 and replace with appropriate response

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,45 @@
 // Business Logic
+var beeopBoopSorry = "";
+numbersArray = [0, 1, 2, 3, 4, 5] // test array
 
 // Counts up from 0 to the user specified number and stores the count in an array
 function incrementCount(number) {
-  var numberArray = [];
+  var numbersArray = [];
   for (var i = 0; i <= number; i++) {
-    numberArray.push(i);
+    numbersArray.push(i);
   }
-  return numberArray
+  return numbersArray;
 }
+
+console.log(incrementCount(15));
+
+// Return Beeps, Boops, and Sorry's
+function beepBooper(digit) {
+  var digit;
+  if (digit === 3) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if (digit === 2) {
+    return "Boop!";
+  } else if (digit === 1) {
+    return "Beep!";
+  } else {
+    return digit;
+  }
+}
+
+// If a number in the array contains the digit 1, return "Beep!"
+// function digitReader(number) {
+//   digits = [];
+//   for ()
+// }
+
+
+
+// User Interface logic
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    var number = $("#input").val();
+    console.log(number)
+  });
+});
