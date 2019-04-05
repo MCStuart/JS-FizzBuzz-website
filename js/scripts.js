e9cc29ca49d197cdeaa4bf6ceb02d33f9a63bca8
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-05 16:26:34 -0700

    throw in the towel on trying to solve this problem without converting to strings. Bummer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,52 @@
 // Business Logic
-
 // Counts up from 0 to the user specified number and stores the count in an array
+var userNumber = 15;
 function incrementCount(userNumber) {
   var numbersArray = [];
   for (var i = 0; i <= userNumber; i++) {
     numbersArray.push(i);
   }
   return numbersArray;
-}
-
+};
+console.log(incrementCount(userNumber));
 // Isolates the digits inside of a number and adds them to an array
-function digitSplitter(number) {
-  var ones = Math.floor((number / 1) % 10);
-  var tens = Math.floor((number / 10) % 10);
-  var hundreds = Math.floor((number / 100) % 10);
-  var digits = [];
-  digits.push(hundreds);
-  digits.push(tens);
-  digits.push(ones);
-  if ((number >= 0) && (number < 10)) {
-    return digits.slice(2);
-  } else if ((number >= 10) && (number < 100)) {
-    return digits.slice(1);
-  } else if ((number >= 100) && (number < 1000)) {
-    return digits;
+function digitFinder(numbers) {
+  for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    if ((number).toString().indexOf("3") != -1) {
+
+    }
   }
 }
 
+  //   var number = numbersArray[i];
+  //   var ones = Math.floor((number / 1) % 10);
+  //   var tens = Math.floor((number / 10) % 10);
+  //   var hundreds = Math.floor((number / 100) % 10);
+  //   var digits = [];
+  //   digits.push(hundreds);
+  //   digits.push(tens);
+  //   digits.push(ones);
+  //   if ((number >= 0) && (number < 10)) {
+  //     return digits.push(number);
+  //     return digits.slice(2);
+  //   }
+  //   else if ((number >= 10) && (number < 100)) {
+  //     return [digits.push(Math.floor((number / 1) % 10)),
+  //       digits.push(Math.floor((number / 10) % 10))];
+  //     return digits.slice(1);
+  //   }
+  //   else if ((number >= 100) && (number < 1000)) {
+  //     return [digits.push(Math.floor((number / 1) % 10)),
+  //       digits.push(Math.floor((number / 10) % 10)),
+  //       digits.push(Math.floor((number / 100) % 10))];
+  //     return digits;
+  //   }
+  // }
+console.log(digitFinder(incrementCount[3]));
+
 // Return Beeps, Boops, and Sorry's
 function beepBooper(digit) {
-  var digit;
   if (digit === 3) {
     return "I'm sorry, Dave. I'm afraid I can't do that.";
   } else if (digit === 2) {
@@ -39,11 +56,11 @@ function beepBooper(digit) {
   } else {
     return digit;
   }
-}
+};
 
-function opensPodBayDoors(numbers) {
+function opensPodBayDoors(number) {
   var outputReturn = "";
-  var numbersArray = incrementCount(userNumber);
+  var numbersArray = incrementCount(number);
   for (var i = 0; i < numbersArray.length; i++) {
     var number = numbersArray[i];
     if (number < 10) {
@@ -51,7 +68,7 @@ function opensPodBayDoors(numbers) {
     }
     else if ((number >=10) && (number <100)) {
       for (var d = 0; d < 2; d++) {
-        var digit = digitSplitter[d];
+        var digit = digitFinder[d];
         if ((digit === 3) || (digit === 2) || (digit === 1)) {
           return beepBooper(digit);
         }
@@ -62,7 +79,7 @@ function opensPodBayDoors(numbers) {
     }
     else if ((numbersArray[i] >= 100) && (numbersArray[i] < 1000)) {
       for (var h = 0; h < 3; h++) {
-        var digit = digitSplitter[h];
+        var digit = digitFinder[h];
         if ((digit === 3) || (digit === 2) || (digit === 1)) {
            return beepBooper(digit);
         } else {
@@ -73,13 +90,13 @@ function opensPodBayDoors(numbers) {
   }
 };
 
-console.log(opensPodBayDoors(5))
-
 // User Interface logic
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     var userNumber = $("#input").val();
-    console.log(userNumber)
+    var output = opensPodBayDoors(userNumber);
+    $('.result').show();
+    $('#outputStr').text(output);
   });
 });
