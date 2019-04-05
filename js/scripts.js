da0099653ba168f67c1427fb7fcfa81789e7ae64
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-05 13:39:51 -0700

    make a mess of everything trying to link up business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 // Business Logic
-var beeopBoopSorry = "";
-
 // Counts up from 0 to the user specified number and stores the count in an array
 function incrementCount(number) {
   var numbersArray = [];
@@ -9,52 +7,46 @@ function incrementCount(number) {
   }
   return numbersArray;
 }
-
 // Isolates the digits inside of a number and adds them to an array
-function digitSplitter(number) {
-  var ones = Math.floor((number / 1) % 10);
-  var tens = Math.floor((number / 10) % 10);
-  var hundreds = Math.floor((number / 100) % 10);
-  var digits = [];
-  digits.push(hundreds);
-  digits.push(tens);
-  digits.push(ones);
-  if ((number >= 0) && (number < 10)) {
-    return digits.slice(2);
-  } else if ((number >= 10) && (number < 100)) {
-    return digits.slice(1);
-  } else if ((number >= 100) && (number < 1000)) {
-    return digits;
+function digitSplitter(numbersArray) {
+  for (var i = numbersArray[0]; i < numbersArray.length; i++) {
+    var ones = Math.floor((numbersArray[i] / 1) % 10);
+    var tens = Math.floor((numbersArray[i] / 10) % 10);
+    var hundreds = Math.floor((numbersArray[i] / 100) % 10);
+    var digits = [];
+    digits.push(hundreds);
+    digits.push(tens);
+    digits.push(ones);
+    if ((numbersArray[i] >= 0) && (numbersArray[i] < 10)) {
+      return digits.slice(2);
+    } else if ((numbersArray[i] >= 10) && (numbersArray[i] < 100)) {
+      return digits.slice(1);
+    } else if ((numbersArray[i] >= 100) && (numbersArray[i] < 1000)) {
+      return digits;
+    }
   }
 }
 
+// Verifies is an array of sperated digits contains any of the proscribed values to replace
+var specifiedNumber = function isSpecifiedNumber(digits) {
+  return (digits === 3) || (digits === 2) || (digits === 1);
+  };
+console.log(digits.some(specifiedNumber));
 
-console.log(digitSplitter(753));
-
-
-
-// Return Beeps, Boops, and Sorry's
-function beepBooper(digit) {
-  var digit;
-  if (digit === 3) {
-    return "I'm sorry, Dave. I'm afraid I can't do that.";
-  } else if (digit === 2) {
-    return "Boop!";
-  } else if (digit === 1) {
-    return "Beep!";
-  } else {
-    return digit;
+// Return Beeps, Boops, and Sorry's for matching values
+function beepBooper(digits) {
+  for (var i = digits[0]; i < digits.length; i++) {
+    if (digit === 3) {
+      return "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (digit === 2) {
+      return "Boop!";
+    } else if (digit === 1) {
+      return "Beep!";
+    } else {
+      return digit;
+    }
   }
-}
-
-
-
-// If a number in the array contains the digit 1, return "Beep!"
-// function digitReader(number) {
-//   digits = [];
-//   for ()
-// }
-
+};
 
 
 // User Interface logic
@@ -62,6 +54,6 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     var number = $("#input").val();
-    console.log(number)
+
   });
 });
