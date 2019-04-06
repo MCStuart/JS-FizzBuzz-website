82e540d9ceefb34ab190fc67b3e7672c9376429e
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-05 18:11:43 -0700

    make everything work with string methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,24 @@
 // Business Logic
 // Counts up from 0 to the user specified number and stores the count in an array
-
 function incrementCount(userNumber) {
   var numbers = [];
   for (var i = 0; i <= userNumber; i++) {
-    numbers.push(i)
+    numbers.push(digitFinder(i.toString()));
   }
-  return (numbers);
+  return numbers;
 };
 
-// Isolates the digits inside of a number and adds them to an array
+// Looks for specific digit values in a number by converting to a string
 function digitFinder(numbers) {
   var outputString = "";
-  if ((number).toString().includes("3") != -1) {
-    return outputString = (outputString + " " + "I'm sorry, Dave. I'm afraid I can't do that.");
-  }
-  else if ((number).toString().includes("2") != -1) {
-    return outputString = (outputString + " " + "Boop!");
-  }
-  else if ((number).toString().includes("1") != -1) {
-    return outputString = (outputString + " " +  "Beep!");
-  }
-  else {
-    return outputString = (outputString + " " + number);
+  if ((numbers).toString().includes("3")) {
+    return outputString = (outputString + " " + "I'm sorry, Dave. I'm afraid I can't do that.")
+  } else if ((numbers).toString().includes("2")) {
+    return outputString = (outputString + " " + "Boop!")
+  } else if ((numbers).toString().includes("1")) {
+    return outputString = (outputString + " " +  "Beep!")
+  } else {
+    return outputString = (outputString + " " + numbers)
   }
 };
 
